5ec1d86d388f34be44d192276991c0682dc6061a
Chao Zhou <e-bread@qq.com>
2012-09-07 17:12:37 +0800

    Add Local Setting Support

--- PomodoroTimer/Utils/Settings.cs
+++ PomodoroTimer/Utils/Settings.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using PomodoroTimer.Extension;
 
 namespace PomodoroTimer
 {
@@ -11,12 +13,33 @@ namespace PomodoroTimer
         public int BreakLength { get; set; }
         public int LongBreakLength { get; set; }
         public int PomodoroLength { get; set; }
+        public bool IsAutoSwich { get; set; }
+
 
         public Settings()
         {
-            BreakLength = 5;
-            LongBreakLength = 15;
-            PomodoroLength = 25;
+            ReLoad();
         }
+
+        public void ReLoad()
+        {
+            var store = ApplicationData.Current.LocalSettings;
+            BreakLength = store.Values.GetValue<int>("breakLen", 5);
+            LongBreakLength = store.Values.GetValue<int>("lbreakLen", 15);
+            PomodoroLength = store.Values.GetValue<int>("workLen", 25);
+            IsAutoSwich = store.Values.GetValue<bool>("autoSwich", true);
+        }
+
+        public void Save()
+        {
+            var store = ApplicationData.Current.LocalSettings;
+            store.Values["breakLen"] = 5;
+            store.Values["lbreakLen"] = 15;
+            store.Values["workLen"] = 25;
+            store.Values["autoSwich"] = IsAutoSwich;
+        }
+        
+
+
     }
 }
