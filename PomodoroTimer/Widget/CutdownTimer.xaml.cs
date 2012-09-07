3ff76b841bc27223b3746c7563b44cc86e02fd97
Chao Zhou <e-bread@qq.com>
2012-09-07 17:17:23 +0800

    Replace DispatcherTimer to WinRTXamlTookit.Tools.BackgroundTimer
    Now, when ui is hide,the timer still run in background. but will stop
    when user stop this application.

--- PomodoroTimer/Widget/CutdownTimer.xaml.cs
+++ PomodoroTimer/Widget/CutdownTimer.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.Tools;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -60,27 +62,47 @@ namespace PomodoroTimer.Widget
             });
         }
 
-        public void Reset()
+        public void Reset(int seconds)
         {
             timer.Stop();
-            recorder.PassedTime = 0;
+            recorder.TotalTime = seconds;
             this.DataContext = new { Dashboard = recorder.RemainingTime };
+        }
+
+        public void Reset()
+        {
+            Reset(recorder.TotalTime);
         }
 
         private void InitializeTimer()
         {
-            timer = new DispatcherTimer();
+            //timer = new DispatcherTimer();
+            timer = new BackgroundTimer();
             timer.Interval = TimeSpan.FromSeconds(1.00);
             timer.Tick += OneSecondPassed;
             this.DataContext = new { Dashboard = 0 };
         }
 
-        private void OneSecondPassed(object sender, object e)
+        private async void OneSecondPassed(object sender, object e)
+        {
+          if (Dispatcher.HasThreadAccess)
+                DoOneSecondPassedWork();
+          else 
+              await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => DoOneSecondPassedWork());
+
+        }
+
+        private void DoOneSecondPassedWork()
         {
             recorder.SecondPassed();
             this.DataContext = new { Dashboard = recorder.RemainingTime };
+            TryToFireComplete();
+        }
 
-            if (recorder.IsTimeOff) {
+        private void TryToFireComplete()
+        {
+            if (recorder.IsTimeOff)
+            {
                 Stop();
                 FireComplete();
             }
@@ -104,7 +126,9 @@ namespace PomodoroTimer.Widget
             timer.Start();
         }
 
-        private DispatcherTimer timer;
+        //private DispatcherTimer timer;
+        private BackgroundTimer timer;
+
         private CutdownTimeRecoder recorder;
     }
 }
