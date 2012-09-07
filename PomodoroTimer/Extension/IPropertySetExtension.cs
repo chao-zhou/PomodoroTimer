fc35d73f02ade78c1b0fe2c04c3e952c96e56e9b
Chao Zhou <e-bread@qq.com>
2012-09-07 17:12:17 +0800

    Add an Extension for Get App Setting Savely
    Now, Can use like
    " Settings.GetValue<Type>(key,defualtValue)"

---
+++ PomodoroTimer/Extension/IPropertySetExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+
+namespace PomodoroTimer.Extension
+{
+    static class IPropertySetExtension
+    {
+        public static T GetValue<T>(this IPropertySet set, string key, T defaul)
+        {
+            if (!set.ContainsKey(key))
+                return defaul;
+
+            return (T)set[key];
+        }
+    }
+}
